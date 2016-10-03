efdd1e1002afc62f6295562cc061d78f1e7e2d9d
kavyamohan <kavya2_mohan@yahoo.in>
2016-10-03 08:11:26 +0530

    second

---
+++ link.py
@@ -0,0 +1,13 @@
+import urllib
+import re
+url=raw_input("enter url:")
+
+f=urllib.urlopen(url)
+d=f.read()
+
+a=re.compile('<img.*src="(.*\.jpg)"')
+n=re.findall(a,d)
+b=open("link1.txt","w")
+for i in n:
+		b.write(url+i+"\n")
+b.close()
